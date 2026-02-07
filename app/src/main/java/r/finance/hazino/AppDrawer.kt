885f63b0116df04dc66c159a1f0b0e71356635ac
RevEngine3r <revengine3r@gmail.com>
2026-02-07 16:25:19 +0330

    AGP 9.0

--- app/src/main/java/r/finance/hazino/AppDrawer.kt
+++ app/src/main/java/r/finance/hazino/AppDrawer.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import r.finance.hazino.R
 
 @Composable
 fun AppDrawer(
